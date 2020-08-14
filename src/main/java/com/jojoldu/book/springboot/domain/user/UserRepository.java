f95bc945b7d260cdfbd3a09a7c400a31e5ba2ed3
ksh030506 <ksh030506@naver.com>
2020-08-15 04:39:52 +0900

    구글 로그인 알고리즘 짜기 어렵다 한번 더 보기 개 어려움 ㅜㅜㅜㅜㅡ,ㅡ,ㅡㅠㅍㅍ 진짜 한번 더 코드 따라 쳐 보기

--- src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
@@ -6,6 +6,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findById(String email);
+    Optional<User> findByEmail(String email);
 
 }
